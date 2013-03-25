c1c3d56c5f16fe996b3a2da627d526ac6836bbd2
Mr. Jenkins <jenkins@aoe-works.de>
2013-03-25 01:53:02 +0100

    Extend specs to check more pages

--- spec/rspec/cacheinfo_spec.rb
+++ spec/rspec/cacheinfo_spec.rb
@@ -1,21 +1,23 @@
 require File.expand_path(File.join(File.dirname(__FILE__), '..', 'spec_helper'))
 
 describe "typo3.org X-Cacheinfo headers" do
-  describe "landingpage headers" do
-    headers = TestHttp.get("/").headers
-    it "should deny logins" do
-      headers.keys.should  include("x-t3cacheinfo")
-      headers["x-t3cacheinfo"].should  =~ /noLoginAllowedInBranch/
-    end
 
-    it "should not contain INT warnings" do
-      headers["x-t3cacheinfo"].should_not =~ /INT/
+  %w{/ /download /about /news}.each do |path|
+    describe "landingpage headers" do
+      headers = TestHttp.get(path).headers
+      it "should deny logins" do
+        headers.keys.should  include("x-t3cacheinfo")
+        headers["x-t3cacheinfo"].should  =~ /noLoginAllowedInBranch/
+      end
+
+      it "should not contain INT warnings" do
+        headers["x-t3cacheinfo"].should_not =~ /INT/
+      end
     end
   end
 
   describe "login sensitve pages" do
     headers = TestHttp.get("/events/add-new-event/").headers
-
     it "should allow logins" do
       headers.keys.should  include("x-t3cacheinfo")
       headers["x-t3cacheinfo"].should  =~ /loginAllowedInBranch/
